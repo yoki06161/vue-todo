44bc58899a1c49d384978349faa75f3bb36bbad0
yoki06161 <yoki06161@naver.com>
2024-02-27 15:25:40 +0900

    feat: 테스트 #3

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,10 @@ function deleteItem(id) {
   // splice(시작위치, 삭제갯수)
   data.items.splice(idx, 1);
 }
+function test() {
+  console.log('11');
+}
+test();
 </script>
 
 <template>
@@ -49,7 +53,7 @@ function deleteItem(id) {
     <h1>Simple to-do list</h1>
     <!-- 자바스크립트 사용시 {{  }} 사용 -->
     <div class="todo_count">
-      완료된 할일: {{ isComplete }} / 할 일: {{ totalItems }}
+      완료: {{ isComplete }} / 할 일: {{ totalItems }}
     </div>
     <div class="todo_add">
       <!-- v-on: 이벤트명 이벤트 연결 디렉티브 -->
